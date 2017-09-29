26d8dc3669db14e7ed970a497e32633641372da9
dealsky <dealsky000@outlook.com>
2017-09-29 21:49:59 +0800

    界面

--- calculator/javascript.js
+++ calculator/javascript.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
-    $("button").click(function() {
-        console.log("f");
-        $(".screen-main").val("1"); 
+    
+    var status = 0;
+    var oAnswerText = $(".screen-main");
+    var buttonNum = $(".button-num");
+    var buttonOp = $(".button-op");
+    
+    buttonNum.click(function() {
+        if($(this).text() == "CE"){
+            oAnswerText.val("0");
+            status = 0;
+        }
+        else if($(this).text() == "0"){
+            if(status == 1){
+                
+            }
+        }
+        else if($(this).text() == "."){
+            
+        }
+        else{
+            
+        }
     });
+    
+    
+    
 });
